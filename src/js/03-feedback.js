89b18f27980828bc268510d0536b6b3bd03bd0e1
romis44 <romankovalchuk44@gmail.com>
2022-12-30 09:30:47 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,8 @@
 import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
-const emailInput = document.querySelector('.feedback-form input');
-const messageInput = document.querySelector('.feedback-form textarea');
 
 const STORAGE_KEY = 'feedback-form-state';
 const STORAGE_GETTTING = localStorage.getItem(STORAGE_KEY);
-const formData = {};
 
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 500));
@@ -13,28 +10,30 @@ form.addEventListener('input', throttle(onFormInput, 500));
 populateTextarea();
 
 function onFormInput(evt) {
-  formData[evt.target.name] = evt.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  // console.log(formData);
+  let inputStorage = localStorage.getItem(STORAGE_KEY);
+  inputStorage = inputStorage ? JSON.parse(inputStorage) : {};
+  inputStorage[evt.target.name] = evt.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(inputStorage));
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
 
   const savedDataObject = {};
-  const savedData = new FormData(form);
-  savedData.forEach((name, value) => {
-    savedDataObject[name] = value;
-  });
+  const savedDataForm = new FormData(form);
+  savedDataForm.forEach((value, name) => (savedDataObject[name] = value));
   console.log(savedDataObject);
-  evt.currentTarget.reset();
+  evt.target.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
-function populateTextarea() {
-  const savedData = JSON.parse(STORAGE_GETTTING);
+function populateTextarea(evt) {
+  let savedData = localStorage.getItem(STORAGE_KEY);
   if (savedData) {
-    emailInput.value = savedData.email;
-    messageInput.value = savedData.message;
+    savedData = JSON.parse(savedData);
+    Object.entries(savedData).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
   }
 }
